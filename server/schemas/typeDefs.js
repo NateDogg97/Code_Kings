9a8173ebe8d986a8ca38bb4d12414297022e2419
rjschroeder <are.jay.schroeder@gmail.com>
2022-11-28 17:33:27 -0600

    finished typeDef for User
    comment on line 3: //can we change the model for User to be "projects:[Project] instead of orders:[Project]"

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,17 @@
 const { gql } = require('apollo-server-express');
 
+//can we change the model for User to be "projects:[Project] instead of orders:[Project]"
 const typeDefs = gql`
     type User {
-
+        _id: ID
+        firstName: String
+        lastName: String
+        email: String
+        orders: [Project]
     }
 
     type Project {
-        
+
     }
 `;
 
