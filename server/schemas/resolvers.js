5cdae100b64254ad37726e5d0e97e024b2b9b362
rjschroeder <are.jay.schroeder@gmail.com>
2022-11-28 21:50:41 -0600

    finished addUser and updateUser mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -69,10 +69,16 @@ const resolvers = {
 
     Mutation: {
         addUser: async (parent, args) => {
-
+            const user = await User.create(args);
+            const token = signToken(user);
+            return {token, user};
         },
         updateUser: async (parent, args, context) => {
-
+            if (context.user) {
+                return await User.findByIdAndUpdate(context.user._id, args, { new: true });
+            }
+        
+            throw new AuthenticationError('Not logged in');
         },
         login: async (parent, {email, password}) => {
             const user = await User.findOne({ email });
