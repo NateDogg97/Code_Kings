c658d9c7b2a7b5a308dd8a6565f44233aacf93c6
rjschroeder <are.jay.schroeder@gmail.com>
2022-11-28 18:58:11 -0600

    removed comments for next meeting

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,5 @@
 const { gql } = require('apollo-server-express');
 
-//can we change the model for User to be "projects:[Project] instead of orders:[Project]"
 const typeDefs = gql`
     type User {
         _id: ID
@@ -47,7 +46,5 @@ const typeDefs = gql`
         ): Auth
     }
 `;
-//need to add  isFinished: Boolean   to Project?
-//if we have a shopping cart, add checkout typedef
 
 module.exports = typeDefs;
