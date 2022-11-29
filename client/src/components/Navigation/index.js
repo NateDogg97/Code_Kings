a2100fb33f9613a58b9d605179988f8e61713b15
NateDogg <natmays97@gmail.com>
2022-11-29 15:41:18 -0600

    starter header

---
+++ client/src/components/Navigation/index.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { Menu, Button } from 'antd';
+import './style.css';
+
+  const items = [
+    {
+      label: <Link to='/login'>
+               Login
+             </Link>,
+      key: 'login',
+      type: 'loggedOut'
+    },
+    {
+      label: <Link to="/signup">
+               Signup
+             </Link>,
+      key: 'signup',
+      type: 'loggedOut'
+    },
+    {
+      label: 'Profile',
+      key: 'SubMenu',
+      children: [
+        {
+          label: 'My profile',
+          key: 'my-profile'
+        },
+        {
+          label: 'Developing',
+          key: 'developing'
+        },
+        {
+          label: 'Settings',
+          key: 'settings'
+        },
+        {
+          label: <Button type="link">Logout</Button>,
+          key: 'logout-btn',
+        }
+      ],
+      type: 'loggedIn'
+    },
+  ];
+
+const Nav = () => {
+  const [current, setCurrent] = useState();
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return <Menu mode="horizontal" onClick={onClick} items={items} current={[current]}/>
+}
+
+export default Nav;
