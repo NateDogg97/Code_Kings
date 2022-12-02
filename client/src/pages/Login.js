1291a11505366d82e887b1697311c8614e2ecb05
rjschroeder <are.jay.schroeder@gmail.com>
2022-12-01 21:04:46 -0600

    added await login code executed onFinish

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import { useMutation } from 'apollo/client';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Button, Form, Input } from 'antd';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Login = () => {
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values);
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN);
 
+  const onFinish = async (values) => {
+    console.log('Received values of form: ', values);
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
   };
+
   return (
     <Form
       name="normal_login"
