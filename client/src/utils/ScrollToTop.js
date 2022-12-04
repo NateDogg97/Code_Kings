230ccf9f9a86a155de09c93cb31fa4c3138b47d9
NateDogg <natmays97@gmail.com>
2022-12-04 17:22:33 -0600

    fixed ScrollToTop

--- client/src/utils/ScrollToTop.js
+++ client/src/utils/ScrollToTop.js
@@ -1,17 +1,12 @@
-import React, { useEffect, Fragment } from 'react';
-import { withRouter } from 'react-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-function ScrollToTop({ history, children }) {
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
   useEffect(() => {
-    const unlisten = history.listen(() => {
-      window.scrollTo(0, 0);
-    });
-    return () => {
-      unlisten();
-    }
-  }, []);
+    window.scrollTo(0, 0);
+  }, [pathname]);
 
-  return <Fragment>{children}</Fragment>;
+  return null;
 }
-
-export default withRouter(ScrollToTop);
