0d62ff65aa2f60b32c8d40be3a21fdcbc9ae37e4
NateDogg <natmays97@gmail.com>
2022-12-05 19:15:19 -0600

    fixed create a project button on the homoe page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Layout, Button } from "antd";
+import Auth from '../utils/auth';
 import ProfileIcon from "../components/ProfileIcon";
 import ProjectCard from "../components/ProjectCard";
+import { redirect } from "react-router-dom";
 
 const { Sider, Content } = Layout;
 
+
 function Home() {
+  const handleProjectButton = () => {
+    Auth.loggedIn ? (
+      window.location.replace('/newproject')
+    ) : (
+      window.location.replace('/login')
+    )
+  }
   return (
     <Layout className="mainLayout">
       <Sider
@@ -14,7 +24,11 @@ function Home() {
         collapsedWidth="0"
       >
         <div className="icon-projPage">
-          <Button className="create-new-proj" type="primary" style={{backgroundColor:'navy',color:'white'}}>
+          <Button 
+            className="create-new-proj" 
+            type="primary" 
+            onClick={handleProjectButton}
+            style={{backgroundColor:'navy',color:'white'}}>
             Create A Project
           </Button>
         </div>
