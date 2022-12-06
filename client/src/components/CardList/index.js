dd1b877a8d65392c283ceee1ad67a1cedca06ffd
hhundley <65366145+hhundley@users.noreply.github.com>
2022-12-06 10:43:55 -0600

    fixing console errors stemming from card list

--- client/src/components/CardList/index.js
+++ client/src/components/CardList/index.js
@@ -2,18 +2,13 @@ import React from 'react';
 import "./style.css";
 import { Link,useNavigate} from 'react-router-dom';
 import { ArrowRightOutlined,PushpinFilled } from '@ant-design/icons';
-import { Avatar, Card } from 'antd';
+import { Avatar, Card, Typography} from 'antd';
 const { Meta } = Card;
+const { Title } = Typography
 
 const CardList = ({
   projects
 }) => {
-  let navigate = useNavigate(); 
-  const routeChange = (project) =>{ 
-    let path = `/projects/${project._id}`; 
-    navigate(path);
-  }
-
   if (!projects.length) {
     return <h3>no open bounties</h3>;
   }
@@ -22,20 +17,16 @@ const CardList = ({
     <div>
       {projects &&
         projects.map((project) => (
-          <Link to={`/projects/${project._id}`}>
-          <div key={project._id} className="cards" onClick={routeChange}>
-          <Card
-            hoverable="true"
-            className="project-card"
-  >
-    <PushpinFilled style={{color:'red',fontSize:'24px'}} />
-    <Meta
-    
-      title={<Link to={`/projects/${project._id}`}>{project.name}</Link>}
-    />
-    <div className="price" style={{color:'grey'}}>${project.price} Bounty </div>
-  </Card>
-          </div> </Link>
+          <Link to={`/projects/${project._id}`} key={project._id}>
+          <div  className="cards">
+          <Card hoverable="true" className="project-card">
+            <PushpinFilled style={{color:'red',fontSize:'24px'}} />
+
+            <Meta title={project.name}/>
+
+              <div className="price" style={{color:'grey'}}>${project.price} Bounty </div>
+          </Card>
+            </div> </Link>
         ))}
     </div>
   );
